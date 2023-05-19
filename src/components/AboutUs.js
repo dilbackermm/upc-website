ef3d87e4c45bc9183cc39e07b9bead037fb3eec1
Dilbacker <mmdilbacker@gmail.com>
2023-05-19 10:35:40 +0530

    about section is mobile responsive now

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -15,18 +15,18 @@ const AboutUs = ({ className = "" }) => {
         <main className={` -pt-16  flex items-center bg-light/75 text-primaryDark w-full min-h-screen 
     pb-0 mb-0 ${className} `} id='aboutus' >
             <Layout className=' bg-transparent z-20 mx-32 xl:mx-12 lg:mx-6'>
-                <AnimatedText text="About Us" className='mb-16 !text-8xl lg:!text-7xl sm:!text-6xl xs:!text-4xl sm:mb-8 ' />
+                <AnimatedText text="About Us" className='mb-16 !text-8xl lg:!text-7xl sm:!text-6xl xs:!text-5xl sm:mb-8 ' />
                 <div className="grid grid-cols-2 lg:flex lg:flex-col gap-16 2xl:gap-9 xl:gap-6">
                     <div className="flex flex-col items-center">
 
 
                         <Image src={aboutLogo} alt="UPC Logo" className="w-full h-auto px-24 " />
-                        <h2 className='Capitalize font-bold text-Dark text-5xl text-center 
+                        <h2 className='Capitalize font-bold text-Dark text-5xl lg:text-4xl text-center 
     leading-[60px] py-6  border-y-2 border-primaryDark '>UNITED PACKAGING CONSOLIDATORS</h2>
-                        <div className="flex justify-between pt-3 ">
-                            <h6 className='font-bold text-xl text-primaryDark  p-5 mx-2' >Accessible</h6>
-                            <h6 className='font-bold text-xl text-primaryDark p-5 mx-2' >Adaptable</h6>
-                            <h6 className='font-bold text-xl text-primaryDark p-5 mx-2' >Affordable</h6>
+                        <div className="flex justify-between  pt-3 ">
+                            <h6 className='font-bold text-xl lg:text-lg sm:text-base text-primaryDark  p-5 mx-2 sm:p-2 sm:mx-1' >Accessible</h6>
+                            <h6 className='font-bold text-xl lg:text-lg sm:text-base text-primaryDark p-5 mx-2 sm:p-2 sm:mx-1' >Adaptable</h6>
+                            <h6 className='font-bold text-xl lg:text-lg sm:text-base text-primaryDark p-5 mx-2 sm:p-2 sm:mx-1' >Affordable</h6>
                         </div>
 
                     </div>
@@ -35,14 +35,14 @@ const AboutUs = ({ className = "" }) => {
                             initial={{ y: 200 }}
                             whileInView={{ y: 0, transition: { duration: 0.9, ease: "easeInOut" } }}
                             viewport={{ once: true }}
-                            className='p-2 text-xl text-dark font-normal leading-[2.25rem] sm:leading-[2rem] text-left sm:text-lg'>
+                            className='p-2 text-xl text-dark font-normal leading-[2.25rem] sm:leading-[2rem] text-left sm:text-lg xs:text-base'>
                             UPC is a fully integrated manufacturing company offering evolving packaging solutions for all your logistic needs.
                         </motion.p>
                         <motion.p
                             initial={{ y: 200 }}
                             whileInView={{ y: 0, transition: { duration: 0.9, ease: "easeInOut" } }}
                             viewport={{ once: true }}
-                            className='p-2 text-xl text-dark font-normal leading-[2.35rem] sm:leading-[2rem] text-left sm:text-lg'>
+                            className='p-2 text-xl text-dark font-normal leading-[2.35rem] sm:leading-[2rem] text-left sm:text-lg xs:text-base'>
                             At UPC, our strength is our capacity to produce &#40;per month&#41;  </motion.p>
                         <motion.div className=' flex justify-between py-5 px-2' >
                             <motion.div
@@ -50,16 +50,16 @@ const AboutUs = ({ className = "" }) => {
                                 whileInView={{ y: 0, transition: { duration: 0.9, ease: "easeInOut" } }}
                                 viewport={{ once: true }}
                                 className="flex flex-col items-center">
-                                <h1 className='text-5xl font-bold'>15,000</h1>
-                                <p className='text-lg font-medium pt-2 text-primaryDark/90 text-center'>  PE Liner bags</p>
+                                <h1 className='text-5xl lg:text-3xl xs:text-2xl font-bold'>15,000</h1>
+                                <p className='text-lg md:text-lg sm:text-base xs:text-sm pt-2 text-primaryDark/90 text-center'>  PE Liner bags</p>
                             </motion.div>
                             <motion.div
                                 initial={{ y: 200 }}
                                 whileInView={{ y: 0, transition: { duration: 0.9, ease: "easeInOut" } }}
                                 viewport={{ once: true }}
                                 className="flex flex-col items-center">
-                                <h1 className='text-5xl font-bold'>6,000</h1>
-                                <p className='text-lg font-medium pt-2 text-primaryDark/90text-center '>woven coated <br />
+                                <h1 className='text-5xl lg:text-3xl xs:text-2xl font-bold'>6,000</h1>
+                                <p className='text-lg md:text-lg sm:text-base xs:text-sm pt-2 text-primaryDark/90text-center '>woven coated <br />
                                     PP/HDP Liners</p>
                             </motion.div>
                             <motion.div
@@ -67,8 +67,8 @@ const AboutUs = ({ className = "" }) => {
                                 whileInView={{ y: 0, transition: { duration: 0.9, ease: "easeInOut" } }}
                                 viewport={{ once: true }}
                                 className="flex flex-col items-center">
-                                <h1 className='text-5xl font-bold'>15,000</h1>
-                                <p className='text-lg font-medium pt-2 text-primaryDark/90 text-center'>Thermal / <br />
+                                <h1 className='text-5xl lg:text-3xl xs:text-2xl font-bold'>15,000</h1>
+                                <p className='text-lg md:text-lg sm:text-base xs:text-sm pt-2 text-primaryDark/90 text-center'>Thermal / <br />
                                     Aluminium Liners</p>
                             </motion.div>
 
@@ -77,7 +77,7 @@ const AboutUs = ({ className = "" }) => {
                             initial={{ y: 200 }}
                             whileInView={{ y: 0, transition: { duration: 0.9, ease: "easeInOut" } }}
                             viewport={{ once: true }}
-                            className='p-2 text-xl text-dark font-normal leading-[2.25rem] sm:leading-[2rem] text-left sm:text-lg'>
+                            className='p-2 text-xl text-dark font-normal leading-[2.25rem] sm:leading-[2rem] text-left sm:text-lg xs:text-base'>
                             We have the capacity to produce over 100,000 dunnage bags per month and offer
                             competitive rates with a focus on quality and commitment to delivery.
                             With over eighty years of combined industry experience, our team provide
--- src/components/WhyUpc.js
+++ src/components/WhyUpc.js
@@ -18,8 +18,8 @@ const Points = ({ point }) => {
         className="flex items-center "
       >
     
-        <Image src={tick} alt="check_icon" className='mx-2 p-0.5' />
-       <h2 className='py-3 pl-3 text-lg text-dark/75 font-medium'>{point}</h2>
+        <Image src={tick} alt="check_icon" className='mx-2 sm:mx-0 p-0.5' />
+       <h2 className='py-3 pl-3 text-lg sm:text-base text-dark/75 font-medium'>{point}</h2>
       </motion.li>
     )
   }
@@ -28,21 +28,21 @@ const Points = ({ point }) => {
 const WhyUpc = () => {
   return (
     <div className="flex py-6  lg:flex-col items-start pb-3">
-         <div className="w-1/2 flex flex-col">
+         <div className="w-1/2 lg:w-full flex flex-col">
         <AnimatedText text="Why UPC?" className='text-primaryDark text-left' />
         <motion.p
                             initial={{ y: 200 }}
                             whileInView={{ y: 0, transition: { duration: 0.9, ease: "easeInOut" } }}
                             viewport={{ once: true }}
-                            className='p-2 pt-9 text-xl text-dark font-normal leading-[2.25rem] sm:leading-[2rem] text-left sm:text-lg'>
+                            className='p-2 pt-9 text-xl  text-dark font-normal leading-[2.25rem] sm:leading-[2rem] text-left sm:text-lg xs:text-base'>
                             At UPC we invest quality time and take great effort in understanding our client-packaging needs
                             and customize our products accordingly to achieve the best results. <br />
                             We are internationally accredited with ISO 9001:2015, FSSC 22000, and BRC certifications.
                         </motion.p>
         </div>
-        <div className="w-1/2">
-<p className='text-dark font-semibold text-xl'>The core values that make clients come back to us for more</p>
-<ul className='pt-9 ml-12 timeline'>
+        <div className="w-1/2 lg:w-full">
+<p className='text-dark font-semibold text-xl sm-text-lg xs:text-base lg:pt-6'>The core values that make clients come back to us for more</p>
+<ul className='pt-9 sm:pt-3 ml-12 sm:ml-3 timeline'>
     <Points point="20-year standing in the industry" />
     <Points point="Excellence in quality and service" />
     <Points point="Proven record of timely delivery" />
