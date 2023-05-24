4ea8a4025d3405e491cb729e2f0ff64b5bb6c7dc
Dilbacker <mmdilbacker@gmail.com>
2023-05-24 17:20:53 +0530

    updating hamburger menue

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -36,7 +36,7 @@ const CustomMobileLink = ({ href, title, className = "", toggle }) => {
     }
 
     return (
-        <button href={href} className={`${className} relative group text-light  my-2`} onClick={handleClick}>
+        <button href={href} className={`${className} relative group text-light text-2xl text-center my-6`} onClick={handleClick}>
             {title}
 
             <span className={`h-[1px] inline-block  bg-light  absolute left-0 -bottom-1
@@ -49,9 +49,9 @@ const CustomMobileLink = ({ href, title, className = "", toggle }) => {
 
 
 
-const Navbar = ( {className = "" }) => {
+const Navbar = ({ className = "" }) => {
 
-   
+
     const [isOpen, setIsOpen] = useState(false)
 
 
@@ -66,12 +66,12 @@ const Navbar = ( {className = "" }) => {
     return (
         <header
             className={`w-full px-32 py-8 font-medium flex items-center justify-between
-            ralative z-10 lg:px-16 md:px-12 sm:px-8 ${className}`} 
+            ralative z-10 lg:px-16 md:px-12 sm:px-8 ${className}`}
         >
-     <div className=' '>
-        <Link href="/" >
-       
-                <Logo />
+            <div className=' '>
+                <Link href="/" >
+
+                    <Logo />
                 </Link>
             </div>
 
@@ -85,7 +85,7 @@ const Navbar = ( {className = "" }) => {
                 ${isOpen ? '-rotate-45 -translate-y-1' : 'translate-y-0.5'} `} ></span>
             </button>
 
-          
+
 
 
             {
@@ -96,7 +96,7 @@ const Navbar = ( {className = "" }) => {
                         animate={{ scale: 1, opacity: 1 }}
                         className=' min-w-[80vw] lg:min-w-[70vw] min-h-[90vh] lg:min-h-[70vh] flex flex-col justify-between z-40 
         itmes-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y1/2
-        bg-primaryDark/90 rounded-2xl backdrop-blur py-32 ' >
+        bg-primaryDark/90 rounded-2xl py-32 backdrop-blur' >
                         <nav className='flex items-center flex-col justif-center'>
                             <CustomMobileLink href="/" title="Home" className='' toggle={handleClick} />
                             <CustomMobileLink href="/#aboutus" title="About" className='' toggle={handleClick} />
@@ -114,7 +114,7 @@ const Navbar = ( {className = "" }) => {
 
 
 
-       
+
 
         </header>
     )
