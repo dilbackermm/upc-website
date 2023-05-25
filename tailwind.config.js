2c44f03338acce08a395e3e203ca6259a98bb950
Dilbacker <mmdilbacker@gmail.com>
2023-05-25 16:54:19 +0530

    fixing resolution

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,7 +32,9 @@ module.exports = {
 
     },
     screens: {
-      "2xl": { max: "1535px" },
+
+      
+      "2xl": { max: "1630px" },
       // => @media (max-width: 1535px) { ... }
 
       xl: { max: "1279px" },
